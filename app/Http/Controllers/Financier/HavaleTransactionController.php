dcc510253c69f2fa3da195a3fce5cbafbf70169b
codedbystability <codedbvystability@gmail.com>
2022-06-07 19:12:43 +0300

    t

--- app/Http/Controllers/Financier/HavaleTransactionController.php
+++ app/Http/Controllers/Financier/HavaleTransactionController.php
@@ -91,12 +91,11 @@ class HavaleTransactionController extends Controller
         $type = $this->transactionTypeRepository->getByKey($typeKey);
 
         $transactions = Transaction::whereIn('client_id', $clientIds)
-            ->when($statusKey === 'waiting', function ($q) use ($status) {
+            ->when($statusKey === 'waiting', function ($q) use ($status, $approvedStatus) {
                 return $q->where('status_id', $status->id)
-                    ->orWhere('status_id',);
+                    ->orWhere('status_id', $approvedStatus->id);
             }, function ($query) use ($status, $approvedStatus) {
-                return $query->where('status_id', $status->id)
-                    ->orWhere('status_id', $approvedStatus);
+                return $query->where('status_id', $status->id);
             })
 
 //            ->where('status_id', $status->id)
