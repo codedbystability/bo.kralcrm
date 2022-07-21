f2d5b0746e9a97eff931291856c5d42c3723e945
codedbystability <codedbvystability@gmail.com>
2022-07-22 00:45:17 +0300

    test

--- app/Http/Controllers/Financier/TransactionController.php
+++ app/Http/Controllers/Financier/TransactionController.php
@@ -210,11 +210,8 @@ class TransactionController extends Controller
 
         $bankInfo = null;
         if ($transaction->account_id) {
-            if ($transaction->transactionable_type === 'App\Models\PaparaDeposit') {
-                $bank = Account::with('accountable')->find($transaction->account_id);
-            } elseif ($transaction->transactionable_type === 'App\Models\HavaleDeposit') {
-                $bank = Account::with('accountable')->find($transaction->account_id);
-            }
+            $bank = Account::with('accountable')->find($transaction->account_id);
+
 
             if ($bankInfo){
                 $bank = array_merge($bank->accountable->toArray());
