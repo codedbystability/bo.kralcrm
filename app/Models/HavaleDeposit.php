095852e849e58f13b26dcf14b9189e589eedb05e
codedbystability <codedbvystability@gmail.com>
2022-07-23 20:09:10 +0300

    currency format

--- app/Models/HavaleDeposit.php
+++ app/Models/HavaleDeposit.php
@@ -17,9 +17,9 @@ class HavaleDeposit extends Model
         'updated_at' => 'date:Y-m-d h:i:s'
     ];
 
-    public function transaction(): \Illuminate\Database\Eloquent\Relations\MorphOne
+    public function transaction(): \Illuminate\Database\Eloquent\Relations\MorphMany
     {
-        return $this->morphOne(Transaction::class, 'transactionable','transactionable_type','transactionable_id','id');
+        return $this->morphMany(Transaction::class, 'transactionable','transactionable_type','transactionable_id','id');
     }
 
     public function account(): \Illuminate\Database\Eloquent\Relations\BelongsTo
