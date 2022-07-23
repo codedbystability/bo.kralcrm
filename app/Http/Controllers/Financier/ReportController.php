bc27cbd94cf20128f009627426ac0eda33623814
codedbystability <codedbvystability@gmail.com>
2022-07-23 18:59:05 +0300

    currency format

--- app/Http/Controllers/Financier/ReportController.php
+++ app/Http/Controllers/Financier/ReportController.php
@@ -239,6 +239,7 @@ class ReportController extends Controller
         $dateFrom = $request->get('date_from');
         $dateTo = $request->get('date_to');
 
+        $t2 = '%' . $request->get('customer_name') . '%';
 
         $transactions = Transaction::when($request->get('website_name'), function ($query) use ($request) {
             return $query->whereHas('website', function ($qq) use ($request) {
@@ -250,10 +251,10 @@ class ReportController extends Controller
             ->whereDate('created_at', ">=", Carbon::createFromFormat('Y-m-d H:i:s', $dateFrom . ' 00:00:00'))
             ->whereDate('created_at', "<=", Carbon::createFromFormat('Y-m-d H:i:s', $dateTo . ' 23:59:59'))
             ->orderBy('id', 'desc')
-            ->when($request->get('customer_name'), function ($query) use ($request) {
+            ->when($request->get('customer_name'), function ($query) use ($request, $t2) {
                 // ISIMLE ARAMA geldiginde
-                return $query->whereHasMorph('transactionable', HavaleDeposit::class, function ($qq) use ($request) {
-                    return $qq->where('havale_deposits.fullname', 'like', '%' . $request->get('customer_name') . '%')->get();
+                return $query->whereHasMorph('transactionable', HavaleDeposit::class, function ($qq) use ($request, $t2) {
+                    return $qq->where('fullname', 'like', $t2)->get();
                 });
 
             })
