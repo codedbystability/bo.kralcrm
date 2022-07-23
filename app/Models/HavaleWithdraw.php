190b0ea3adf78139066dd36ef4262fccaeae0af6
codedbystability <codedbvystability@gmail.com>
2022-07-23 19:25:36 +0300

    currency format

--- app/Models/HavaleWithdraw.php
+++ app/Models/HavaleWithdraw.php
@@ -16,8 +16,8 @@ class HavaleWithdraw extends Model
         'updated_at' => 'date:Y-m-d h:i:s'
     ];
 
-    public function transaction(): \Illuminate\Database\Eloquent\Relations\morphOne
+    public function transaction(): \Illuminate\Database\Eloquent\Relations\MorphMany
     {
-        return $this->morphOne(Transaction::class, 'transactionable');
+        return $this->morphMany(Transaction::class, 'transactionable');
     }
 }
