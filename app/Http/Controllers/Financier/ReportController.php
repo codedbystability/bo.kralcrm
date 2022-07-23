03871d8d9310898ba12d54de5239a02397327fd3
codedbystability <codedbvystability@gmail.com>
2022-07-23 17:50:24 +0300

    currency format

--- app/Http/Controllers/Financier/ReportController.php
+++ app/Http/Controllers/Financier/ReportController.php
@@ -292,7 +292,7 @@ class ReportController extends Controller
             })
             ->whereDate('created_at', ">=", Carbon::createFromFormat('Y-m-d H:i:s', $dateFrom . ' 00:00:00'))
             ->whereDate('created_at', "<=", Carbon::createFromFormat('Y-m-d H:i:s', $dateTo . ' 23:59:59'))
-            ->with('website', 'client', 'status', 'type', 'method', 'currency')
+            ->with('website', 'client', 'status', 'type', 'method', 'currency','transactionable')
             ->orderBy('id', 'desc')
             ->paginate(20)
             ->appends($request->except('page'));
