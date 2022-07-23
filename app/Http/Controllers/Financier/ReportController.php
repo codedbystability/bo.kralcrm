1a49052d15989fa52f05ec26f847250387a8e1e0
codedbystability <codedbvystability@gmail.com>
2022-07-23 19:13:01 +0300

    currency format

--- app/Http/Controllers/Financier/ReportController.php
+++ app/Http/Controllers/Financier/ReportController.php
@@ -253,7 +253,7 @@ class ReportController extends Controller
             ->orderBy('id', 'desc')
             ->when($request->get('customer_name'), function ($query) use ($txt) {
                 // ISIMLE ARAMA geldiginde
-                return $query->whereHasMorph('transactionable',function ($qq) use ($txt ) {
+                return $query->whereHasMorph('transactionable', [HavaleDeposit::class], function ($qq) use ($txt ) {
                     return $qq->where('fullname', 'like', "%" . $txt  . "%")->get();
                 });
 
